9ac302bff673f55bfe5671c2320f7dcb473c5df5
Nate Holland <nathanhollanddev@gmail.com>
2025-09-05 13:30:53 -0400

    More test work

--- cli/src/test/java/dev/buildcli/cli/commands/project/InitCommandTest.java
+++ cli/src/test/java/dev/buildcli/cli/commands/project/InitCommandTest.java
@@ -5,6 +5,7 @@ import java.nio.file.Files;
 
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.io.TempDir;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import dev.buildcli.cli.utilsfortest.LogbackExtension;
@@ -20,25 +21,20 @@ import static org.junit.jupiter.api.Assertions.*;
 @ExtendWith({MockitoExtension.class, LogbackExtension.class})
 class InitCommandTest {
 
+    @TempDir
     private File tempDir;
-    
-    @BeforeEach
-    void setup() throws Exception {
-        tempDir = Files.createTempDirectory("startDir").toFile();
-    }
-
-    @AfterEach
-    void tearDown() {
-        for (File file : tempDir.listFiles()) {
-            file.delete();
-        }
-        tempDir.delete();
-    }
+    private InitCommand initCommand;
 
     @Test
-    void executeCreatesCorrectly() {
-        String[] args = {"-n", "testDirName"};
-        int exitCode = new CommandLine(new GenerateCommand()).execute(args);
+    void executeCreatesCorrectly(@TempDir File tempDirFile) {
+        
+        tempDir = new File(tempDir, "StartDir");
+        initCommand = new InitCommand();
+        new CommandLine(initCommand).execute("-n", "TestRootDir", "-j", "17", "-t", "false");
+
+        
+        
+
     }
 
 }
